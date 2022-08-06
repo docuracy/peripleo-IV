210d2f6c1fe28e38d328dcfa9bd8499d63f006fb
docuracy <42514781+docuracy@users.noreply.github.com>
2022-08-06 16:04:08 +0100

    Fixed duplicate files.

--- webpack.config.js
+++ webpack.config.js
@@ -6,7 +6,7 @@ module.exports = {
   entry: './src/index.jsx',
   output: {
     filename: 'peripleo-IV.js',
-    path: path.resolve(__dirname, 'public'),
+    path: path.resolve(__dirname, 'docs'),
   },
   resolve: {
     extensions: ['.js', '.jsx'],
